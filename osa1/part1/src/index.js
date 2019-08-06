ca9265a45d69b8dbd78f3c640dbf16a7e4e0db2a
Visa Hannula <visa.hannula@viha.fi>
2019-08-07 02:38:08 +0300

    More testing with example app and Yarn updates

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -13,18 +13,44 @@ const Hello = ({ name, age }) => {
 }
 
 const Footer = (props) => (
-    <div id="{props.id || 'footer'">
+    <div id={props.id || 'footer'}>
         <p>This is from the footer, you know that {props.name}.</p>
     </div>
 )
 
-const Display = ({ counter}) => <div>{ counter }</div>;
+const Display = ({ counter }) => <div>Counter: { counter }</div>;
 const Button = ({ text, func }) => <button onClick={func}>{ text }</button>;
+
+class ButtonWithState extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = { clicks: 0 };
+        this.handleClick = () => {
+            this.setState({ clicks: this.state.clicks + 1 });
+        }
+    }
+
+    render() {
+        return (
+            <button onClick={ this.handleClick }>{ `${this.props.text}: ${this.state.clicks}` }</button>
+        )
+    }
+}
+
+const ButtonWithStateHooks = props => {
+    const [clicks, handleClick] = useState(0);
+    
+    props.func("Hi, it's ButtonWithStateHooks. Clicks: " + clicks);
+    
+    return (
+        <button onClick={ () => handleClick(clicks + 1) }>{ `${props.text}: ${clicks}` }</button>
+    )
+} 
 
 const App = () => {
     const [ counter, setCounter ] = useState(0);
 
-    console.log("Testiviesti");
+    console.log("Testiviesti" + counter);
     const now = Date.now();
     const a = 10;
     const b = 20;
@@ -33,16 +59,26 @@ const App = () => {
 
     const setValue = (value=0) => () => setCounter(value);
 
+    const printToConsole = text => console.log(text);
+
     return (
         <>
             <p>Yellow World!</p>
             <Display counter={ counter } />
             <Hello name={uname} age={age} />
-            <p>This was brought to you on UNIX TIME: {now}</p>
+
             <p>{a} + {b} = {a + b}</p>
-            <Button func={setValue(counter + 1)} text="Plus" />
-            <Button func={setValue(counter - 1)} text="Minus" />
-            <Button func={setValue()} text="Zero" />
+
+            <Button func={ setValue(counter + 1) } text="Plus" />
+            <Button func={ setValue(counter - 1) } text="Minus" />
+            <Button func={ setValue() } text="Zero" />
+
+            <p>Clicktests</p>
+            <ButtonWithState text={"Left"} />
+            <ButtonWithState text={"Right"} />
+            <ButtonWithStateHooks text={"Alt"} func={ printToConsole } />
+
+            <p>This was brought to you on UNIX time: <code>{now}</code></p>
             <Footer name={uname} />
         </>
     )
