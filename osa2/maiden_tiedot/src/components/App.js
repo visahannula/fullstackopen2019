383b2ba1a9732392c96b481620a614bc30d75046
Visa Hannula <visa.hannula@viha.fi>
2021-03-26 01:49:50 +0200

    Refactor filter func. Support single country

--- osa2/maiden_tiedot/src/components/App.js
+++ osa2/maiden_tiedot/src/components/App.js
@@ -3,107 +3,78 @@ import './App.css';
 
 import SearchInput from './SearchInput';
 import CountryLister from './CountryLister';
+import { SingleCountryContainer } from './SingleCountry';
 
 import RestCountries from '../RestCountries';
 
 
 const restCountries = new RestCountries();
-restCountries.filters = ["name", "alpha3Code", "nativeName"];
-
-/**
- * Form the list of values based on a filter value.
- * 
- * @param {String} searchInputValue Input field value to use for filtering
- * @param {Array} countryListValues Array of all the countries
- * @returns {Array} Array of values
- */
-/*const currDispList = (searchInputValue, countryListValues) => {
-  if (searchInputValue.isEmpty) {
-    //console.log(countryListValues);
-    return countryListValues;
-  } else {
-    return countryListValues.filter((item) => {
-      console.log("Found ? : ", item.name.indexOf(searchInputValue));
-      return item.name.indexOf(searchInputValue) > -1;
-    }
-    )
-  }
-}
-*/
 
 function App() {
-  const [searchInputValue, setSearchInputValue] = useState(
-    { inputValue: "", isEmpty: true }
-  );
   // All countries
   const [countryListValues, setCountryListValues] = useState(
     { countries: [] }
   );
   // Currently displayed countries
-  const [currListValues, setCurrListValues] = useState(countryListValues);
+  const [currListValues, setCurrListValues] = useState([]);
+  const [isCountryListFetched, setIsCountryListFetched] = useState(false);
 
-  // initial get for the list of countries
+  // initial get for the list of all countries
   useEffect(() => {
     console.log("Getting data");
 
+    restCountries.filters = ["name", "alpha3Code", "nativeName"];
     restCountries.all(response => {
-      console.log("Effect got: " + response[0]);
-      
       if (response) {
         setCountryListValues({ countries: response });
-        setCurrListValues({ countries: response });
+        setCurrListValues(response);
+        setIsCountryListFetched(true);
+      } else {
+        console.log("Cannot get countries.");
       }
     });
   },
     []
   );
 
-  // sideeffect for search input field
-  useEffect(() => {
-    console.log("Effect hook: ", searchInputValue);
+  const inputValueHandler = (input) => {
+    console.log("In inputvalue handler. ", input);
 
-    let inValue = { countries: [] };
-
-    if (searchInputValue.isEmpty) {
-      inValue = countryListValues;
+    if (input.isEmpty) {
+      setCurrListValues(countryListValues.countries); // show everything if nothing set
     } else {
-      inValue.countries =
-        countryListValues
-          .countries
-          .filter(country => 
-            /* Search input from two country item props */
-            ["name", "nativeName", "alpha3Code"].reduce((prev, curr, index, arr) =>
-              country[arr[index]]
-              .toLowerCase()
-              .indexOf(
-                searchInputValue
-                  .inputValue
-                  .toLowerCase()
-              ) > -1 || prev
-            , false)
-          );
+      setCurrListValues(
+        filterFunc(countryListValues.countries, ["name", "nativeName", "alpha3Code"], input.inputValue)
+      );
     }
+  }
 
-    setCurrListValues(inValue);
-  },
-    [setCurrListValues, searchInputValue, countryListValues]
-  );
+  /**
+   * Search input value from each country properties.
+   */
+  const filterFunc = (countryObjArray, countryPropKeyArray, valueToSearch) =>
+    countryObjArray.filter(arrItem =>
+      countryPropKeyArray.some(currKey =>
+        arrItem[currKey]
+          .toLowerCase()
+          .indexOf(valueToSearch.toLowerCase()) > -1)
+    );
 
   return (
     <div className="App">
       <div id="searchContainer">
         <p>Search for countries by writing below</p>
         <SearchInput
-          setValueCallback={setSearchInputValue}
+          isInputEnabled={ isCountryListFetched } setValueCallback={inputValueHandler}
         />
       </div>
-      <div id="countryListContainer">
-        {<CountryLister list={currListValues.countries} />}
-      </div>
       <div id="singleCountryContainer">
-        { /* Single country information */}
+        <SingleCountryContainer country={isCountryListFetched ? "FIN" : false }></SingleCountryContainer>
       </div>
-      <div>The data for this service is provided by <a href="https://restcountries.eu/" target="_blank" rel="noopener noreferrer">https://restcountries.eu/</a> Thank you!</div>
+      <div id="countryListContainer">
+        {<CountryLister list={currListValues} />}
+      </div>
+      <div id="footer">The data for this service is provided by <a href="https://restcountries.eu/" target="_blank" rel="noopener noreferrer">https://restcountries.eu/</a> Thank you!</div>
     </div>
   );
 }
