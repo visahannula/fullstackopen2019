6e0d14b7b9690bbd629d7e877d69cb79d44e2fe5
Visa Hannula <visa.hannula@viha.fi>
2021-03-29 03:22:42 +0300

    Add debounce timer

--- osa2/maiden_tiedot/src/components/SearchInput.js
+++ osa2/maiden_tiedot/src/components/SearchInput.js
@@ -4,24 +4,26 @@ import './SearchInput.css';
 
 export const SearchInput = ({ setValueCallback, isInputEnabled }) => {
     const [visibleInputValue, setVisibleInputValue] = useState("");
-    const [internalInputValue, setInternalInputValue] = useState("");
+    const [timeoutID, settimeoutID] = useState(0);
 
     const checkSetInputValue = ({ target: { value } }) => {
         const valueTrim = value.trim();
-
-        if (valueTrim !== "") console.log("Input value is empty. Stored: ", internalInputValue);
-
-        if (value !== internalInputValue) {
+        if (valueTrim !== "") console.log("Input value is empty. Stored: ", visibleInputValue);
+        if (value !== visibleInputValue) {
             console.log("Input value changed. ", value);
-            setInternalInputValue(value);
             setVisibleInputValue(value);
-            setValueCallback(
-                {
-                    inputValue: valueTrim,
-                    isEmpty: !!!valueTrim
-                }
-            );
+            clearTimeout(timeoutID); // debouncing
+            settimeoutID(setTimeout(updateInputState, 500, value));
         }
+    }
+
+    const updateInputState = (inValue, callback) => {
+        setValueCallback(
+            {
+                inputValue: inValue,
+                isEmpty: !!!inValue
+            }
+        );
     }
 
     return (
