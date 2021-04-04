83fd41b4e260f420c7e566cf0e4a6c2376618ee3
Visa Hannula <visa.hannula@viha.fi>
2021-04-04 22:30:26 +0300

    Refactor to smaller components, add list item expansion

--- osa2/maiden_tiedot/src/components/CountryLister.js
+++ osa2/maiden_tiedot/src/components/CountryLister.js
@@ -13,13 +13,45 @@ export const StatusCircle = ({ isRed }) => {
     return isRed ? red : green;
 }
 
-// TODO: Add container element which decides which to render (list or single element)
+export const FullCountryInfo = ({ countryData }) => {
+    const { region, capital, population, topLevelDomain, languages } = countryData;
 
-export const ExpandedSingleCountry = ({ country: { name, alpha3Code, nativeName }, listId }) => {
+    return (
+        <div className="countryInformation">
+            <div className="leftSide">
+                <h3>Country information</h3>
+                <ul>
+                    <li>World region: {region}</li>
+                    <li>Capital: {capital}</li>
+                    <li>Population: {new Intl.NumberFormat().format(population)}</li>
+                    <li>Top level domain: {topLevelDomain}</li>
+                    <li>Spoken languages: {languages.map(lang => lang['name']).join(", ")}</li>
+                </ul>
+                <div className="weatherContainer">
+                    <h4>Current weather in <b>{capital}</b></h4>
+                    <WeatherMapContainer city={capital}></WeatherMapContainer>
+                </div>
+            </div>
+
+            <div className="rightSide">
+                <div className="flagAndMap">
+                    <img className="flag" src={countryData['flag']} alt="flag" />
+                    <MapIframe
+                        latitude={countryData['latlng'][0]}
+                        longitude={countryData['latlng'][1]}
+                        className="mapIframe">
+                    </MapIframe>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export const ExpandedSingleCountry = ({ country: { name, alpha3Code, nativeName } }) => {
     const [isFullData, setIsFullData] = useState(false);
     const [countryData, setCountryData] = useState(undefined);
 
-    console.log("ExpandedSingleCoutry, data given: ", name, alpha3Code, nativeName, listId);
+    console.log("ExpandedSingleCoutry, data given: ", name, alpha3Code, nativeName);
 
     useEffect(() => {
         function getGetCountryData() {
@@ -38,120 +70,74 @@ export const ExpandedSingleCountry = ({ country: { name, alpha3Code, nativeName 
         [alpha3Code]
     );
 
-    console.log("IsFullData: ", isFullData);
-    console.log("CountryData: ", countryData);
-
-    let countryElement;
-
-    if (isFullData && countryData) { // THIS IS COUNTRY WE HAVE ALL THE INFO AVAILABLE
-        const { region, capital, population, topLevelDomain, languages } = countryData;
-
-        countryElement = (
-            <div className="card" id={"country_" + listId}>
-                <div className="cardContainer">
-                    <div className="cardHeader">
-                        <div className="countryCode">{alpha3Code}</div>
-                        <div className="cardText">{name}, {nativeName}</div>
-                    </div>
-                    <div className="countryInformation">
-                        <div className="leftSide">
-                            <h3>Country information</h3>
-                            <ul>
-                                <li>World region: {region}</li>
-                                <li>Capital: {capital}</li>
-                                <li>Population: {new Intl.NumberFormat().format(population)}</li>
-                                <li>Top level domain: {topLevelDomain}</li>
-                                <li>Spoken languages: {languages.map(lang => lang['name']).join(", ")}</li>
-                            </ul>
-                            <div className="weatherContainer">
-                                <h4>Current weather in <b>{capital}</b></h4>
-                                <WeatherMapContainer city={capital}></WeatherMapContainer>
-                            </div>
-                        </div>
-
-                        <div className="rightSide">
-                            <div className="flagAndMap">
-                                <img className="flag" src={countryData['flag']} alt="flag" />
-                                <MapIframe
-                                    latitude={countryData['latlng'][0]}
-                                    longitude={countryData['latlng'][1]}
-                                    className="mapIframe">
-                                </MapIframe>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    } else { // NO SPECIFIC INFO AVAILABLE FOR THIS COUNTRY (YET)
-        countryElement = (
-            <div className="card" id={"country_" + listId
-            }>
-                <div className="cardContainer">
-                    <div className="cardHeader">
-                        <div className="countryCode">{alpha3Code}</div>
-                        <div className="cardText">{name}, {nativeName}</div>
-                    </div>
-                </div>
-                <div className="waiting">Please wait, getting more information.</div>
-            </div >
-        );
-    }
-
-    return countryElement;
-}
-
-export const SingleCountryListItem = ({ country: { name, alpha3Code, nativeName }, listId, clickHandler }) => {
-    //console.log(name, listId);
+    //console.log("IsFullData: ", isFullData);
+    //console.log("CountryData: ", countryData);
 
     return (
-        <div className="card" id={"country_" + listId}>
-            <div className="cardContainer" onClick={() => clickHandler({alpha3Code})}> {/* Fetch unique alpha3Code */ }
+        (isFullData && countryData)
+            ? <FullCountryInfo countryData={countryData} />
+            : <div className="waiting">Please wait, getting more information.</div>
+    );
+}
+
+/**
+ * Country List item, this is kind of a header also.
+ * Will conditionally render expanded info (children).
+ */
+export const SingleCountryListItem = ({ country: { name, alpha3Code, nativeName }, children, showFull }) => {
+    //console.log(name, listId);
+    const [isFullInfoVisible, setIsFullInfoVisible] = useState(showFull);
+
+    const handleClickShowFullToggle = () => {
+        setIsFullInfoVisible(!isFullInfoVisible);
+    }
+
+    useEffect(() => {
+        setIsFullInfoVisible(showFull);
+    }, [showFull]);
+
+    return (
+        <div className="card">
+            <div className="cardContainer" onClick={handleClickShowFullToggle}> {/* Fetch unique alpha3Code */}
                 <div className="cardHeader">
                     <div className="countryCode">{alpha3Code}</div>
                     <div className="cardText">{name}, {nativeName}</div>
                 </div>
+                {isFullInfoVisible && children}
             </div>
         </div>
     );
 }
 
 
-
 export const CountryLister = ({ list, setValueCallback }) => {
     let isListEmpty = list.length < 1;
     let emptyListMessageEl = (<span>No countries found. Try searching with other words.</span>);
 
-    console.info("List", list);
+    console.info("List of countries to list: ", list);
 
     let countryListContainer = () => emptyListMessageEl;
 
-    if (list.length === 1) {
-        countryListContainer = ([country = list[0]]) =>
+    countryListContainer = list => list.map((value, index) => (
+        <SingleCountryListItem
+            country={value}
+            key={value['alpha3Code']}
+            showFull={list.length === 1 ? true : false}
+        >
             <ExpandedSingleCountry
-                country={country}
-                key={country['alpha3Code']}
-                listId={0}
-            />
-    } else if (list.length > 1) {
-        countryListContainer = list => list.map((value, index) =>
-            <SingleCountryListItem
                 country={value}
                 key={value['alpha3Code']}
-                listId={index}
-                clickHandler={country => setValueCallback(country)}
             />
-        );
-    }
+        </SingleCountryListItem>
+    ));
 
     return (
         <>
-            { list.length === 1
+            {list.length === 1
                 ? <div
                     className="clickToFullList"
                     onClick={() => setValueCallback({ isEmpty: true })}
-                    >⬅ Back to list
-                  </div>
+                >⬅ Back to list</div>
                 : <p><StatusCircle isRed={isListEmpty} /> Found {list.length} countries.</p>
             }
             <div id="list-container">{countryListContainer(list)}</div>
